5e4b43bc4367cd9f51cf6111e495ef34ad1c2cdb
Ibukun450 <145903890+Ibukun450@users.noreply.github.com>
2026-05-26 21:32:44 +0100

    Script file

---
+++ script/script.js
@@ -0,0 +1,20 @@
+
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.add("active");
+    overlay.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+});
